76cb32941c3e151e9ce08c21fb42bbe999318f5b
Fergus Oubre <fergpo@MacBookPro.attlocal.net>
2025-01-23 22:55:32 -0500

    Adding ability to add a user

---
+++ lambdas/users/db/queries.go
@@ -0,0 +1,25 @@
+package db
+
+const (
+	InsertUserQuery = `
+		INSERT INTO USER (
+			user_id,
+			company_id,
+			first_name,
+			last_name,
+			password,
+			email,
+			admin,
+			created_at
+		) VALUES (
+			$1, $2, $3, $4, $5, $6, $7, $8
+		) RETURNING user_id
+	`
+
+	// Add more queries as needed
+	SelectUserQuery = `
+		SELECT user_id, company_id, first_name, last_name, email, admin, created_at
+		FROM USER
+		WHERE user_id = $1
+	`
+)
