c129384c0fad37e0d745cd1718ff5fed5bfe42c6
Fergus Oubre <fergpo@MacBookPro.attlocal.net>
2025-02-01 20:48:27 -0500

    Adding get company cost

--- lambdas/resources/router.go
+++ lambdas/resources/router.go
@@ -10,35 +10,35 @@ import (
 )
 
 func Router(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	path := req.Path
+	resouce := req.Resource
 	method := req.HTTPMethod
 
 	switch {
 	// RESOURCES
-	case path == "/resources" && method == http.MethodGet:
+	case resouce == "/resources" && method == http.MethodGet:
 		return handlers.GetAllResources(ctx, req)
 
-	case path == "/companies/{companyUuid}/resources" && method == http.MethodGet:
+	case resouce == "/companies/{companyUuid}/resources" && method == http.MethodGet:
 		return handlers.GetAllCompanyResources(ctx, req)
 
-	case path == "/companies/{companyUuid}/resources/{resourceUuid}" && method == http.MethodGet:
+	case resouce == "/companies/{companyUuid}/resources/{resourceUuid}" && method == http.MethodGet:
 		return handlers.GetCompanyResource(ctx, req)
 
-	case path == "/companies/{companyUuid}/resources/{resourceUuid}" && method == http.MethodPut:
+	case resouce == "/companies/{companyUuid}/resources/{resourceUuid}" && method == http.MethodPut:
 		return handlers.UpdateCompanyResource(ctx, req)
 
-	case path == "/companies/{companyUuid}/resources/{resourceUuid}" && method == http.MethodDelete:
+	case resouce == "/companies/{companyUuid}/resources/{resourceUuid}" && method == http.MethodDelete:
 		return handlers.RemoveCompanyResource(ctx, req)
 
-	case path == "/companies/{companyUuid}/resources/{resourceUuid}/data" && method == http.MethodGet:
+	case resouce == "/companies/{companyUuid}/resources/{resourceUuid}/data" && method == http.MethodGet:
 		return handlers.GetCompanyResourceData(ctx, req)
 
-	case path == "/companies/{companyUuid}/resources/{resourceUuid}/cost" && method == http.MethodGet:
+	case resouce == "/companies/{companyUuid}/resources/{resourceUuid}/cost" && method == http.MethodGet:
 		return handlers.GetCompanyResourceCost(ctx, req)
 
 	//DEFAULT
 	default:
-		log.Printf("Incorrect path: %v", path)
+		log.Printf("Incorrect resouce: %v", resouce)
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusNotFound,
 			Body:       `{"error":"Not Found resources lambda"}`,
