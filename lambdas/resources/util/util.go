d8aa79d0659d5271dc4eca457f9bb9f8e787af6f
Fergus Oubre <fergpo@MacBookPro.attlocal.net>
2025-02-22 21:01:22 -0500

    testing time array

--- lambdas/resources/util/util.go
+++ lambdas/resources/util/util.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	// "fmt"
+	"log"
 	"resources/types"
 	"time"
 )
@@ -11,74 +12,51 @@ func EvenlyBucketMetrics(metrics []types.Metric, c int) ([]types.EvenMetric, err
 		return nil, nil
 	}
 
-	unit := ""
-	bucketDur := time.Duration(c) * time.Minute
 	var buckets []types.EvenMetric
+	historicMinutes := 24 * 60
+	numBuckets := historicMinutes / c
+	startTime := time.Now().Add(time.Duration(-historicMinutes) * time.Minute)
+	// evaluatedTime := startTime
 
 	uniqueMetricType := make(map[string][]types.Metric)
 	for _, m := range metrics {
 		uniqueMetricType[m.Name] = append(uniqueMetricType[m.Name], m)
 	}
 
-	for key, value := range uniqueMetricType {
 
-		unit = value[0].Unit
-		firstTime := value[0].Timestamp
-		curStart := firstTime
-		curEnd := curStart.Add(bucketDur)
-		var accValue float64
-		var accDuration time.Duration
-
-		for _, m := range value {
-
-			rowDuration := time.Duration(m.Aggregate) * time.Minute
-			valuePerMinute := m.Value / float64(m.Aggregate)
-			remaining := rowDuration
-
-			for remaining > 0 {
-				// Calculate how much time remains in the current bucket.
-				timeLeftInBucket := curEnd.Sub(curStart) - accDuration
-
-				if remaining <= timeLeftInBucket {
-					// If the remaining row duration fits in the current bucket, assign it.
-					accValue += valuePerMinute * remaining.Minutes()
-					accDuration += remaining
-					remaining = 0
-				} else {
-					// Fill the current bucket with as much as possible.
-					accValue += valuePerMinute * timeLeftInBucket.Minutes()
-					accDuration += timeLeftInBucket
-					remaining -= timeLeftInBucket
-				}
-
-				// Finalize the current bucket if it's full.
-				if accDuration >= bucketDur {
-					buckets = append(buckets, types.EvenMetric{
-						Name:  key,
-						Start: curStart,
-						End:   curEnd,
-						Value: accValue,
-						Unit:  unit,
-					})
-					// Move to the next bucket.
-					curStart = curEnd
-					curEnd = curStart.Add(bucketDur)
-					accValue = 0
-					accDuration = 0
-				}
-			}
-		}
-
-		if accDuration > 0 {
-			buckets = append(buckets, types.EvenMetric{
-				Name:  key,
-				Start: curStart,
-				End:   curStart.Add(accDuration),
-				Value: accValue,
-				Unit:  unit,
-			})
-		}
+	var timeArray []time.Time
+	evaluatedTime := startTime
+	for i:=0; i<=numBuckets; i++ {
+		timeArray = append(timeArray, evaluatedTime)
+		evaluatedTime = evaluatedTime.Add(time.Duration(c) * time.Minute)
 	}
+	log.Printf("timeMap: %v", timeArray)
+
+
+	// for key, value := range uniqueMetricType {
+	// 	for _, m := range value {
+	// 		metricStartTime := m.Timestamp
+	// 		bucketValue := (m.Value / float64(m.Aggregate)) * float64(c)
+
+
+
+	// 		if (evaluatedTime.After(metricStartTime)|| evaluatedTime.Equal(metricStartTime)) &&
+	// 		   (evaluatedTime.Before(metricEndTime) || evaluatedTime.Equal(metricEndTime)) {
+
+	// 		}
+
+	// 		// 	buckets = append(buckets, types.EvenMetric{
+	// 		// 		Name:  key,
+	// 		// 		Start: curStart,
+	// 		// 		End:   curEnd,
+	// 		// 		Value: accValue,
+	// 		// 		Unit:  unit,
+	// 		// 	})
+
+	// 		// }
+	// 	}
+	// }
+
 
 	return buckets, nil
 }
