43bbbaad98f204bf0c06839391fc6d0a5a63bb83
Fergus Oubre <fergpo@MacBookPro.attlocal.net>
2025-03-01 20:07:25 -0500

    testing fetching policy

--- lambdas/companies/go.mod
+++ lambdas/companies/go.mod
@@ -9,9 +9,11 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go v1.55.6 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
--- lambdas/companies/handlers/roles.go
+++ lambdas/companies/handlers/roles.go
@@ -3,17 +3,34 @@ package handlers
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"net/url"
 
 	// "log"
 	// "net/http"
-
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/iam"
 
 	"companies/util"
 )
 
 type ArnAttachRequest struct {
 	Arn string `json:"arn"`
+}
+
+// Policy represents the overall policy document.
+type Policy struct {
+	Version   string      `json:"Version"`
+	Statement []Statement `json:"Statement"`
+}
+
+// Statement represents a single statement in the policy.
+type Statement struct {
+	Effect   string      `json:"Effect"`
+	Action   string      `json:"Action"`
+	Resource interface{} `json:"Resource"`
 }
 
 func AttachCompanyRoleArn(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -24,5 +41,58 @@ func AttachCompanyRoleArn(ctx context.Context, req events.APIGatewayProxyRequest
 	if json_err != nil {
 		return util.InputErrorResponse("Invalid JSON"), nil
 	}
-	return util.SuccessResponse(arnAttachRequest.Arn), nil
+
+	sess, err := session.NewSession()
+	if err != nil {
+		fmt.Printf("failed to create session: %v", err)
+	}
+
+	iamClient := iam.New(sess)
+
+	roleOutput, err := iamClient.GetRole(&iam.GetRoleInput{
+		RoleName: aws.String("alpha-metric-processor-role-616fl245 "),
+	})
+	if err != nil {
+		fmt.Printf("failed to create session: %v", err)
+	}
+
+	encodedPolicy := aws.StringValue(roleOutput.Role.AssumeRolePolicyDocument)
+	decodedPolicy, err := url.QueryUnescape(encodedPolicy)
+	if err != nil {
+		fmt.Printf("failed to create session: %v", err)
+	}
+
+	return util.SuccessResponse(decodedPolicy), nil
+}
+
+func (p *Policy) addResource(newARN string) error {
+	for i, stmt := range p.Statement {
+		// Check if the Action (or any other indicator) matches what we expect.
+		// In this case, we target the sts:AssumeRole statement.
+		if stmt.Action == "sts:AssumeRole" {
+			switch resource := stmt.Resource.(type) {
+			case string:
+				// Convert single string to slice
+				p.Statement[i].Resource = []string{resource, newARN}
+			case []interface{}:
+				// Convert []interface{} to []string while appending newARN
+				var resources []string
+				for _, v := range resource {
+					if s, ok := v.(string); ok {
+						resources = append(resources, s)
+					} else {
+						fmt.Printf("unexpected resource type")
+					}
+				}
+				resources = append(resources, newARN)
+				p.Statement[i].Resource = resources
+			case []string:
+				// Directly append if already a slice of strings
+				p.Statement[i].Resource = append(resource, newARN)
+			default:
+				fmt.Printf("unknown type for Resource")
+			}
+		}
+	}
+	return nil
 }
